beaae51841c2fd5b287e14d5b4b5f77e391ca980
TanKaka123 <hongtan1422002@gmail.com>
2023-12-25 03:12:34 +0700

    config

--- config/connection-database.js
+++ config/connection-database.js
@@ -3,7 +3,7 @@ const DB_MONGODB_CONNECTION_URL = process.env.DB_MONGODB_CONNECTION_URL;
 
 async function connectToDatabase() {
   try {
-    await mongoose.connect(DB_MONGODB_CONNECTION_URL);
+    await mongoose.connect("mongodb+srv://username123:oq6f3HVT41jdDRH9@restaurant-booking.a3sw4mu.mongodb.net/?retryWrites=true&w=majority");
     console.log("Connected to DB");
   } catch (err) {
     console.error("Failed to connect to the database:", err);
