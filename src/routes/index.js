60e6b6f17dcbd8fbe77ab0062506ee2aec1bee87
TanKaka123 <hongtan1422002@gmail.com>
2023-12-25 02:21:11 +0700

    add default root

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,11 @@ const routes = (app) => {
   app.use("/api/v1/restaurant", restaurantRouter);
   app.use("/api/v1/user", userRouter);
   app.use("/api/v1/reservation", reservationRouter);
+  app.use("/", (req, res) => {
+    res.status(404).json({
+      message: "Hello, world!",
+    });
+  });
 };
 
 module.exports = routes;
